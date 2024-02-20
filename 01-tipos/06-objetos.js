15845a76257bd856dd2f669ecb26cc042ead587a
Jodatrash <106109828+JonathanPaCruz@users.noreply.github.com>
2024-02-19 22:30:53 -0600

    feat: add file 06-objetos.js

---
+++ 01-tipos/06-objetos.js
@@ -0,0 +1,23 @@
+// personaje de tv
+let name = "Jonathan";
+let course = "Java";
+let edad = 16;
+
+// Se conoce como obajeto literal
+let personaje = {
+    // llave - valor
+    // Propiedad.
+    name: "Jonathan",
+    course: "Java",
+    edad: 16,
+};
+console.log(personaje);
+console.log(personaje.name);
+console.log(personaje['course']);
+
+personaje.edad = 25;
+
+personaje['edad'] = 25;
+
+delete personaje.course;
+console.log(personaje);
