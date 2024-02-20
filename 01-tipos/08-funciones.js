8253026314414d6ea93c5c2779776011ace2d90b
Jodatrash <106109828+JonathanPaCruz@users.noreply.github.com>
2024-02-19 23:07:12 -0600

    feat: add file 08-funciones.js

---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,18 @@
+// Crear funciones en js palabra reservada function seguido del nombre de la funcion.
+function saludar(){
+    console.log('Hola Mundo!');
+}
+// Llamar una funcion
+saludar();
+
+// Calculando valores
+function sumar(){
+    return 2 + 100;
+}
+
+// Metodo 1 para trabajar de esta manera.
+let result = sumar();
+console.log(result);
+
+// Metodo 2 para obtener la informacion.
+console.log(sumar()); 
