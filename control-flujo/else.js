666547db891c6fbb45899f9df357748755f880a3
Jodatrash <106109828+JonathanPaCruz@users.noreply.github.com>
2024-02-22 12:44:18 -0600

    feat: add else.js

---
+++ control-flujo/else.js
@@ -0,0 +1,6 @@
+let edad = 10;
+if (edad > 17) {
+  console.log("Es mayor de edad");
+} else if (edad > 13) {
+  console.log("Usuario debe ser acompañado por un adulto");
+} else console.log("No puede ingresar");
