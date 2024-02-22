bef1287f7fd08d1d6fc526ec987422d067f7534a
Jodatrash <106109828+JonathanPaCruz@users.noreply.github.com>
2024-02-22 14:18:12 -0600

    feat: add for-of.js

---
+++ control-flujo/for-of.js
@@ -0,0 +1,12 @@
+let animals = ["Dragon", "Perro", "Gato"];
+//Para iterar un listado de items
+for (let animal of animals) {
+  console.log(animal);
+}
+
+// Hace lo mismo que forof pero con while
+let i = 0;
+while (i < animals.length) {
+  console.log(animals[i]);
+  i++;
+}
